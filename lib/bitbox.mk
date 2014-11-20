2884b43492f570a5038c41e22579a3fede50c142
Adrien Destugues <pulkomandy@gmail.com>
2014-11-20 22:34:16 +0100

    Fix emulator target.
    $? is the list of files that were modified since last build. $^ is the
    list of all required files, which is what we need here.

--- lib/bitbox.mk
+++ lib/bitbox.mk
@@ -201,5 +201,4 @@ endif
 
 
 $(NAME)_emu: $(GAME_C_FILES) $(BITBOX)/lib/emulator.c $(GAME_BINARY_FILES:%=$(BUILD_DIR)/%.c) $(addprefix $(BITBOX)/lib/, $(ENGINE_FILES))
-	gcc -Og -DEMULATOR  $(GAME_C_OPTS) $? -I$(BITBOX)/lib/ -g -Wall -std=c99 $(HOSTLIBS) `sdl-config --cflags --libs` -o $(NAME)_emu
-
+	gcc -Og -DEMULATOR  $(GAME_C_OPTS) $^ -I$(BITBOX)/lib/ -g -Wall -std=c99 $(HOSTLIBS) `sdl-config --cflags --libs` -o $(NAME)_emu
