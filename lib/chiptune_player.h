793029a0e088144b12a6a2d6a7a9f2fc3d86fbf0
Adrien Destugues <pulkomandy@gmail.com>
2015-06-24 18:18:46 +0200

    chiptune player: expose things needed by the chiptracker
    May also be useful for other applications:
    * sine table
    * frequency/notes table
    * runcmd()
    * internal structure of player channels (can be directly controlled)
    * some less interesting internal variables

--- lib/chiptune_player.h
+++ lib/chiptune_player.h
@@ -1,3 +1,42 @@
+/* Simple soundengine for the BitBox
+ * Copyright 2014-2015, Adrien Destugues <pulkomandy@pulkomandy.tk>
+ * Copyright 2007, Linus Akesson
+ * Based on the "Hardware Chiptune" project
+ */
+
+struct unpacker {
+	uint16_t	nextbyte;
+	uint8_t	buffer;
+	uint8_t	bits;
+};
+
+struct channel {
+	struct unpacker		trackup;
+	uint8_t			tnum;
+	int8_t			transp;
+	uint8_t			tnote;
+	uint8_t			lastinstr;
+	uint8_t			inum;
+	uint16_t			iptr;
+	uint8_t			iwait;
+	uint8_t			inote;
+	int8_t			bendd;
+	int16_t			bend;
+	int8_t			volumed;
+	int16_t			dutyd;
+	uint8_t			vdepth;
+	uint8_t			vrate;
+	uint8_t			vpos;
+	int16_t			inertia;
+	uint16_t			slur;
+};
+
+extern const uint16_t freqtable[];
+extern const int8_t sinetable[];
+
+extern struct channel channel[4];
+extern uint8_t trackwait;
+
 /** Initialize the player to play the given song.
  * - Oscillators are reset to silence
  * - Song position is reset to 0
@@ -11,3 +50,9 @@ void ply_init(const uint8_t songlen, const unsigned char* song);
  * song is running.
  */
 void ply_update(); // Call this once per frame.
+
+/** Directly run a command on a channel.
+ *
+ * Can be used to play sound effects.
+ */
+void runcmd(uint8_t ch, uint8_t cmd, uint8_t param);
