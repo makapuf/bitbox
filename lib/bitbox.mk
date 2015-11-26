0dfa763eb1184f4cb252b4b6c49fa211f2e467af
Makapuf <makapuf2@gmail.com>
2015-11-27 00:59:34 +0100

    improved bitbox.mk
    Modularized the bitbox.mk
    
    now sdl has a real build environment instead of cramming
    everything in a line, test also and we will be able to add oter targets
    all is still done out of tree, in build/bitbox build/sdl and build/test
    
    current projects should build the same.

--- lib/bitbox.mk
+++ lib/bitbox.mk
@@ -1,10 +1,14 @@
-# BITBOX environment variable should point to the base bitbox source  dir (where this file is)
+# Bitbox Makefile helper.
 
-# DEFINE in outside makefile
+# BITBOX environment variable should point to the base bitbox source  dir (where this file is)
+# DEFINES in outside makefile
 #   NAME : name of the project
 #   GAME_C_FILES c files of the project
-#	GAME_BINARY_FILES : binary files to embed as part of the main binary ROM
+#   GAME_BIN_FILES : files to embed as part of the main binary ROM. Note: you can use GAME_BIN_FILES=$(wildcard data/*) 
+
 #   GAME_C_OPTS : C language options. Those will be used for the ARM game as well as the emulator.
+#		DEFINES : PROFILE		- enable profiling (red line / pixels onscreen)
+
 #		- define with whatever defines are needed with -DXYZ CFLAGS .
 #		  they can be used to define specific kernel resolution. 
 #   	  In particular, define one of VGAMODE_640, VGAMODE_800, VGAMODE_320 or VGA_640_OVERCLOCK
@@ -12,249 +16,232 @@
 #
 #       - Other specific flags : 
 #             NO_USB,       - when you don't want to use USB input related function)
+#			  NO_AUDIO
 #             USE_SDCARD,   - when you want to use or compile SDcard or fatfs related functions in the game 
 #             USE_ENGINE,   - when you want to use the engine
-#             USE_SD_SENSE  - enabling this will disable being used on rev2 !
-#			  DISABLE_ESC_EXIT - for the emulator only, disable quit when pressing ESC
-#             KEYB_FR       - enable AZERTY keybard mapping
-#			  PROFILE		- enable profiling (red line / pixels onscreen)
-
+#             USE_SAMPLER=1
+#             USE_CHIPTUNES 
 #   Simple mode related : 
 #        VGA_SIMPLE_MODE=0 .. 12 (see simple.h for modes)
-#   Simple Sampler : 
-#        USE_SAMPLER=1
 
-CC = arm-none-eabi-gcc
-LD = arm-none-eabi-gcc
-OBJCOPY = arm-none-eabi-objcopy
+# More arcane options : 
+#     USE_SD_SENSE  - enabling this will disable being used on rev2 !
+#     DISABLE_ESC_EXIT - for the emulator only, disable quit when pressing ESC
+#     KEYB_FR       - enable AZERTY keybard mapping
 
-DEFINES += -DARM_MATH_CM4  -DAUDIO 
 
-# USB defines
-ifdef NO_USB
-DEFINES += -DNO_USB
-else
-DEFINES += -DUSE_USB_OTG_HS -DUSE_EMBEDDED_PHY -DUSE_USB_OTG_FS
-endif
+# just the names of the targets in a generic way
+BITBOX_TGT:=$(NAME).elf
+SDL_TGT:=$(NAME) 
+TEST_TGT:=$(NAME)_test
 
-DEFINES += -DUSE_STDPERIPH_DRIVER 
+all: $(SDL_TGT) $(BITBOX_TGT:%.elf=%.bin) 
 
-MCU = -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -march=armv7e-m 
-C_OPTS = -std=c99 \
-	$(MCU) \
-	-D__FPU_USED=1 \
-	-mlittle-endian \
-	-I$(BITBOX)/lib/ \
-	-I$(BITBOX)/lib/CMSIS/Include \
-	-I$(BITBOX)/lib/StdPeriph \
-	-O3 -g \
-	-Wall \
-	-ffast-math \
-	-fsingle-precision-constant \
-	-ffunction-sections -fdata-sections \
-	-funroll-loops \
-	-fomit-frame-pointer 
-	#-funroll-all-loops \
-	#-Werror \
+# --- option-only targets (independent from target)
 
-LD_FLAGS = $(MCU) -nostartfiles
-LIBS =	-lm -lc
+BUILD_DIR := build
 
-SOURCE_DIR =.
+VPATH=.:$(BITBOX)/lib:$(BITBOX)/lib/StdPeriph
 
-BUILD_DIR =build
-LIB_SOURCE_DIR =$(BITBOX)/lib
-LIB_STD_SOURCE_DIR =$(BITBOX)/lib/StdPeriph
+INCLUDES=-I$(BITBOX)/lib/ -I$(BITBOX)/lib/CMSIS/Include -I$(BITBOX)/lib/StdPeriph
 
-# replace with linker_raw if you want to overwrite bootloader
-LINKER_SCRIPT = $(BITBOX)/lib/Linker_loader.ld
-FLASH_START = 0x08004000
-#LINKER_SCRIPT = lib/Linker_raw.ld
-#FLASH_START = 0x08000000
+# language specific (not specific to target)
+C_OPTS =  -std=c99 -g -Wall -ffast-math -fsingle-precision-constant -ffunction-sections -fdata-sections -funroll-loops -fomit-frame-pointer 
 
-KERNEL_FILES += startup.c system.c board.c \
-	new_vga.c bitbox_main.c audio.c \
-	stm32f4xx_gpio.c \
-	stm32f4xx_rcc.c \
-	stm32f4xx_tim.c \
-	misc.c 
+LD_FLAGS = -Wl,--gc-sections 
+AUTODEPENDENCY_CFLAGS=-MMD -MF$(@:.o=.d) -MT$@
 
-ifndef NO_USB
-	KERNEL_FILES += usb_bsp.c \
-	usb_core.c \
-	usb_hcd.c \
-	usb_hcd_int.c \
-	usbh_core.c \
-	usbh_hcs.c \
-	usbh_hid_core.c \
-	usbh_hid_keybd.c \
-	usbh_hid_mouse.c \
-	usbh_hid_gamepad.c \
-	usbh_hid_parse.c \
-	usbh_ioreq.c \
-	usbh_stdreq.c \
-	stm32fxxx_it.c 
-endif 
+# functional defines for all targets. -D will be expanded after. 
+DEFINES = 
 
-# fatfs related files
-ifdef USE_SDCARD
-KERNEL_FILES += fatfs/stm32f4_lowlevel.c fatfs/stm32f4_discovery_sdio_sd.c fatfs/ff.c fatfs/diskio.c
-KERNEL_FILES += stm32f4xx_sdio.c stm32f4xx_dma.c 
-endif 
+# --- Engines (not target specific)
 
-# Engines related - engines are libs that stay on top of the kernel
-# so they are included in the emulator. Most are optional
+GAME_C_FILES += evt_queue.c 
 
-# - events
-ENGINE_FILES += evt_queue.c
 # - tiles & sprites
 ifdef USE_ENGINE
-ENGINE_FILES +=  blitter.c blitter_btc.c blitter_sprites.c blitter_tmap.c
+GAME_C_FILES +=  blitter.c blitter_btc.c blitter_sprites.c blitter_tmap.c
 endif
 
 # - simple modes
 ifdef VGA_SIMPLE_MODE
-GAME_C_OPTS += -DVGA_SIMPLE_MODE=$(VGA_SIMPLE_MODE)
-ENGINE_FILES +=  simple.c fonts.c
-# those modes require kernel mode 800x600
-ifneq ($(filter $(VGA_SIMPLE_MODE),1 2),)
-GAME_C_OPTS += -DVGAMODE_800
-endif 
-
-# 800x600 O/C 192 to achieve this mode
-ifneq ($(filter $(VGA_SIMPLE_MODE),11 ),)
-GAME_C_OPTS += -DVGAMODE_800_OVERCLOCK
-endif 
-
-# 400x300 mode
-ifeq ($(VGA_SIMPLE_MODE),4)
-GAME_C_OPTS += -DVGAMODE_400
-endif
-# 320x240 mode
-ifeq ($(VGA_SIMPLE_MODE),5)
-GAME_C_OPTS += -DVGAMODE_320
-endif
-
-endif 
+DEFINES += VGA_SIMPLE_MODE=$(VGA_SIMPLE_MODE)
+GAME_C_FILES += simple.c fonts.c
+endif # vga kernel mode defined in kconf.h 
 
 # - simple sampler
 ifdef USE_SAMPLER
-ENGINE_FILES += sampler.c
+GAME_C_FILES += sampler.c
 endif
 
 # - chiptune engine
 ifdef USE_CHIPTUNE 
 $(warning the chiptune engine is about to change. Please change to the chiptune.h file)
-ENGINE_FILES += chiptune_engine.c chiptune_player.c
+GAME_C_FILES += chiptune_engine.c chiptune_player.c
 endif
 
+# TODO : put me in kconf ...
+# - simple modes
+ifdef VGA_SIMPLE_MODE
+# those modes require kernel mode 800x600
+ifneq ($(filter $(VGA_SIMPLE_MODE),1 2),)
+DEFINES += VGAMODE_800
+endif 
 
-C_FILES = $(LIB_FILES) $(GAME_C_FILES) $(KERNEL_FILES) $(ENGINE_FILES)
-S_FILES = memcpy-armv7m.S
+# 800x600 O/C 192 to achieve this mode
+ifneq ($(filter $(VGA_SIMPLE_MODE),11 ),)
+DEFINES += VGAMODE_800_OVERCLOCK
+endif 
 
-OBJS = $(C_FILES:%.c=$(BUILD_DIR)/%.o) $(S_FILES:%.S=$(BUILD_DIR)/%.o) $(GAME_BINARY_FILES:%=$(BUILD_DIR)/%.o)
+# 400x300 mode
+ifeq ($(VGA_SIMPLE_MODE),4)
+DEFINES += VGAMODE_400
+endif
 
-ALL_CFLAGS = $(C_OPTS) $(DEFINES) $(CFLAGS) $(GAME_C_OPTS)
-ALL_LDFLAGS = $(LD_FLAGS) -Wl,-T,$(LINKER_SCRIPT),--gc-sections
+# 320x240 mode
+ifeq ($(VGA_SIMPLE_MODE),5)
+DEFINES += VGAMODE_320
+endif
+endif  # simple modes
 
-AUTODEPENDENCY_CFLAGS=-MMD -MF$(@:.o=.d) -MT$@
+# -- Target-specifics 
+MCU=-mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -march=armv7e-m -mlittle-endian -nostartfiles
+$(BITBOX_TGT): CC=arm-none-eabi-gcc
+$(BITBOX_TGT): C_OPTS += -O3 $(MCU)
+$(BITBOX_TGT): LD_FLAGS += $(MCU)
+$(BITBOX_TGT): DEFINES += __FPU_USED=1  
 
-all: $(NAME).bin $(NAME) $(EXTRA_FILES)
-
-upload: $(NAME).bin
-	openocd -f interface/stlink-v2.cfg -f target/stm32f4x_stlink.cfg \
-	-c init -c "reset halt" -c "stm32f2x mass_erase 0" \
-	-c "flash write_bank 0 $(NAME).bin 0" \
-	-c "reset run" -c shutdown
-
-debug: $(NAME).elf
-	arm-none-eabi-gdb $(NAME).elf \
-	--eval-command="target extended-remote :4242"
-
-stlink: $(NAME).bin
-	#arm-eabi-gdb $(NAME).elf --eval-command="target ext :4242"
-	st-flash write $(NAME).bin $(FLASH_START)
-
-# using dfu util	
-dfu: $(NAME).bin
-	dfu-util -D $< --dfuse-address 0x8000000 -a 0
-
-# double colon to allow extra cleaning
-clean::
-	rm -rf $(BUILD_DIR) $(NAME).elf $(NAME).bin $(NAME) $(NAME)_test
-
-$(NAME).bin: $(NAME).elf
-	$(OBJCOPY) -O binary $^ $@
-	chmod -x $@
-
-$(NAME).elf: $(OBJS)
-	$(LD) $(ALL_LDFLAGS) -o $@ $^ $(LIBS)
-	chmod -x $@
-
-.SUFFIXES: .o .c .S
-
-$(BUILD_DIR)/%.o: $(LIB_SOURCE_DIR)/%.c
-	@mkdir -p $(dir $@)
-	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
-
-$(BUILD_DIR)/%.o: $(LIB_SOURCE_DIR)/%.S
-	@mkdir -p $(dir $@)
-	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
-
-$(BUILD_DIR)/%.o: $(LIB_STD_SOURCE_DIR)/%.c
-	@mkdir -p $(dir $@)
-	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
-
-# ---------- data embedding 
-$(BUILD_DIR)/binaries.h: 
-	# should transform it using make syntax.
-	@mkdir -p $(dir $@)
-	echo "// AUTO GENERATED BY bitbox.mk DO NOT MODIFY " > $@
-	echo "// -- binaries " > $@
-	echo $(GAME_BINARY_FILES) | sed s/[/\.]/_/g | sed "s/ /\n/g" | sed "s/.*/extern const unsigned char \0[];/" >> $@ 
-	echo "// -- lengths " >> $@
-	echo $(GAME_BINARY_FILES) | sed s/[/\.]/_/g | sed "s/ /\n/g" | sed "s/.*/extern const unsigned int \0_len;/">> $@ 
-
-$(BUILD_DIR)/%.c: % 
-	@mkdir -p $(dir $@)
-	$(info * embedding $< as $@)
-	xxd -i $< | sed "s/unsigned/const unsigned/" > $@
-
-$(BUILD_DIR)/%.o: $(BUILD_DIR)/%.c
-	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
-
-# ---------------------------------
-
-
-$(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.c
-	@mkdir -p $(dir $@)
-	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
-
-$(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.cpp
-	@mkdir -p $(dir $@)
-	$(CC) -fno-exceptions -fno-rtti -Wno-multichar $(subst -std=c99,-std=c++11,$(ALL_CFLAGS)) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
-
-
-$(BUILD_DIR)/%.o: $(SOURCE_DIR)/%.S
-	@mkdir -p $(dir $@)
-	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
-
--include $(OBJS:.o=.d)
-#--- Emulator
+ifndef NO_BOOTLOADER 
+$(BITBOX_TGT): LD_FLAGS+=-Wl,-T,$(BITBOX)/lib//Linker_loader.ld
+$(BITBOX_TGT): FLASH_START = 0x08004000
+else 
+$(BITBOX_TGT): LD_FLAGS+=-Wl,-T,$(BITBOX)/lib//Linker_raw.ld
+$(BITBOX_TGT): FLASH_START = 0x08000000
+endif 
 
 HOST = $(shell uname)
 ifeq ($(HOST), Haiku)
   HOSTLIBS =
 else
-  HOSTLIBS = -lm
+  HOSTLIBS = -lm -lc -lstdc++
+endif
+$(SDL_TGT) $(TEST_TGT): CC=gcc
+$(SDL_TGT) $(TEST_TGT): DEFINES += EMULATOR
+$(SDL_TGT): C_OPTS += -Og 
+$(SDL_TGT): C_OPTS != sdl-config --cflags
+$(SDL_TGT): HOSTLIBS += $(shell sdl-config --libs)
+
+KERNEL_SDL=emulator.c
+KERNEL_TEST=tester.c
+KERNEL_BITBOX=board.c startup.c system.c bitbox_main.c
+
+# -- Optional AND target specific
+
+ifndef NO_VGA
+KERNEL_BITBOX += new_vga.c
+else 
+DEFINES += NO_VGA
 endif
 
+# fatfs related files
+SDCARD_FILES := fatfs/stm32f4_lowlevel.c fatfs/stm32f4_discovery_sdio_sd.c fatfs/ff.c fatfs/diskio.c stm32f4xx_sdio.c stm32f4xx_dma.c
+ifdef USE_SDCARD
+DEFINES += USE_SDCARD
+KERNEL_BITBOX += $(SDCARD_FILES)
+endif 
 
-$(NAME): $(GAME_C_FILES) $(GAME_CPP_FILES) $(BITBOX)/lib/emulator.c  $(GAME_BINARY_FILES:%=$(BUILD_DIR)/%.c) $(addprefix $(BITBOX)/lib/, $(ENGINE_FILES))
-	gcc -Og -DEMULATOR  $(GAME_C_OPTS) $^ -I$(BITBOX)/lib/ -g -Wall -std=c99 $(HOSTLIBS) `sdl-config --cflags --libs` -lstdc++ -o $(NAME)
+# USB defines
+ifdef NO_USB
+DEFINES += NO_USB
+else 
+$(BITBOX_TGT): DEFINES += USE_USB_OTG_HS USE_EMBEDDED_PHY USE_USB_OTG_FS USE_STDPERIPH_DRIVER
 
-$(NAME)_test: $(GAME_C_FILES) $(GAME_CPP_FILES) $(BITBOX)/lib/tester.c  $(GAME_BINARY_FILES:%=$(BUILD_DIR)/%.c) $(addprefix $(BITBOX)/lib/, $(ENGINE_FILES))
-	gcc -Og -DEMULATOR  $(GAME_C_OPTS) $^ -I$(BITBOX)/lib/ -g -Wall -std=c99 $(HOSTLIBS) -lstdc++ -o $(NAME)_test
+KERNEL_BITBOX += usb_bsp.c usb_core.c usb_hcd.c usb_hcd_int.c \
+	usbh_core.c usbh_hcs.c usbh_stdreq.c usbh_ioreq.c \
+	usbh_hid_core.c usbh_hid_keybd.c usbh_hid_mouse.c usbh_hid_gamepad.c \
+	usbh_hid_parse.c \
+	stm32fxxx_it.c stm32f4xx_gpio.c stm32f4xx_dma.c misc.c stm32f4xx_rcc.c
+
+
+endif
+
+ifdef NO_AUDIO
+DEFINES+=NO_AUDIO
+else
+KERNEL_BITBOX += audio.c
+endif
+
+# --- binaries as direct object linking + binaries.h from all data in /data directory (if present)
+
+# see http://stackoverflow.com/questions/17265950/linking-arbitrary-data-using-gcc-arm-toolchain
+DATA_OBJ:= $(patsubst %,$(BUILD_DIR)/data/%.o,$(GAME_BINARY_FILES))
+$(BUILD_DIR)/data/%.o: data/%
+	@mkdir -p $(dir $@)
+	objcopy -I binary -O elf32-little $^ $@
+
+# ----------------------------------------------
+
+GAME_C_FILES+=$(GAME_BINARY_FILES:%=$(BUILD_DIR)/%.c)
+
+$(BUILD_DIR)/binaries.h: $(GAME_BINARY_FILES)
+#	_binary_<plop>_start , _binary_<plop>_end , _binary_<plop>_len
+	# should transform it using make syntax.
+	@mkdir -p $(dir $@)
+	echo "// AUTO GENERATED BY bitbox.mk DO NOT MODIFY " > $@
+	echo "// -- binaries " > $@
+	echo $^ | sed s/[/\.]/_/g | sed "s/ /\n/g" | sed "s/.*/extern const unsigned char \0[];/" >> $@ 
+	echo "// -- lengths " >> $@
+	echo $^ | sed s/[/\.]/_/g | sed "s/ /\n/g" | sed "s/.*/extern const unsigned int \0_len;/">> $@ 
+
+$(BUILD_DIR)/%.c: % 
+	@mkdir -p $(dir $@)
+	$(info * embedding $^ as $@)
+	xxd -i $^ | sed "s/unsigned/const unsigned/" > $@
+
+# --- Compilation pattern rules
+
+ALL_CFLAGS = $(DEFINES:%=-D%) $(C_OPTS) $(INCLUDES) $(GAME_C_OPTS)
+
+# must put 4 rules and not only once since multi target pattern rules are special :)
+$(BUILD_DIR)/bitbox/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
+$(BUILD_DIR)/sdl/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
+$(BUILD_DIR)/test/%.o: %.c
+	@mkdir -p $(dir $@)
+	$(CC) $(ALL_CFLAGS) $(AUTODEPENDENCY_CFLAGS) -c $< -o $@
+
+%.bin: %.elf
+	arm-none-eabi-objcopy -O binary $^ $@
+	chmod -x $@
+
+# --- Targets 
+
+$(SDL_TGT): $(GAME_C_FILES:%.c=$(BUILD_DIR)/sdl/%.o) $(KERNEL_SDL:%.c=$(BUILD_DIR)/sdl/%.o)
+	$(CC) $(LD_FLAGS) $^ -o $@ $(HOSTLIBS)
+
+$(TEST_TGT): $(GAME_C_FILES:%.c=$(BUILD_DIR)/test/%.o) $(KERNEL_TEST:%.c=$(BUILD_DIR)/test/%.o)
+	$(CC) $(LD_FLAGS) $^ -o $@ $(HOSTLIBS) 
+
+$(BITBOX_TGT): $(GAME_C_FILES:%.c=$(BUILD_DIR)/bitbox/%.o) $(KERNEL_BITBOX:%.c=$(BUILD_DIR)/bitbox/%.o)
+	$(CC) $(LD_FLAGS) $^ -o $@ $(HOSTLIBS) 
+	chmod -x $@
+
+# --- Helpers
 
 test: $(NAME)_test
 	./$(NAME)_test
+
+debug: $(BITBOX_TGT)
+	arm-none-eabi-gdb $@ --eval-command="target extended-remote :4242"
+
+
+stlink: $(NAME).bin
+	st-flash write $^ $(FLASH_START)
+
+
+# double colon to allow extra cleaning
+clean::
+	rm -rf $(BUILD_DIR)$(BITBOX_TGT) $(NAME).bin $(SDL_TGT) $(TEST_TGT)
