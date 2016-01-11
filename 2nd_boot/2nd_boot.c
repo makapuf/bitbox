7939ccd2d505a59a6d98934d120a0fb9268b8096
Makapuf <makapuf2@gmail.com>
2016-01-11 23:35:39 +0100

    fixed a const warning (+whitespace)

--- 2nd_boot/2nd_boot.c
+++ 2nd_boot/2nd_boot.c
@@ -1,6 +1,6 @@
 // bootloader
 
-// 2 - second stage bootloader from SD can load a larger bootloader 
+// 2 - second stage bootloader from SD can load a larger bootloader
 // with customizable menu, UI, USB, whatever.
 
 
@@ -10,7 +10,7 @@
  */
 
 #include <string.h>
-#include <stdlib.h> // qsort 
+#include <stdlib.h> // qsort
 // #include "system.h" // system_init
 #include "bitbox.h"
 #include "fatfs/ff.h"
@@ -50,7 +50,7 @@ FATFS fs32;
 	}
 
 	char flash_message[32];
-	int flash_start_write(FIL *file) { 
+	int flash_start_write(FIL *file) {
 		frame_started = vga_frame;
 		strcpy(flash_message,"Faking flashing.");
 		return 0;
@@ -60,12 +60,12 @@ FATFS fs32;
 		if (frame_started && vga_frame-frame_started>3*60)
 			strcpy(flash_message,"** Done! Please press reset **");
 	}
-#else 
+#else
 
-#define ROOT_DIR "" 
+#define ROOT_DIR ""
 
 /*
-#include "stm32f4xx.h" 
+#include "stm32f4xx.h"
 // Code stolen from "matis"
 // http://forum.chibios.org/phpbb/viewtopic.php?f=2&t=338
 void jump(uint32_t address)
@@ -100,7 +100,7 @@ void jump(uint32_t address)
     Jump_To_Application();
 }
 */
-#endif 
+#endif
 
 
 extern const uint8_t font_data [256][16];
@@ -113,17 +113,17 @@ char filenames[MAX_FILES][13]; // 8+3 +. + 1 chr0
 #define ICON_SIZE (ICON_W*ICON_W/8) // 128x128 1 bit data
 
 int icon_x, icon_y;
-uint8_t icon_data[ICON_SIZE]; // 2KB b&w 128x128 data 
+uint8_t icon_data[ICON_SIZE]; // 2KB b&w 128x128 data
 // pixel on(i,j) = data[i*16+j/8]&(1<<(7-j%8)))
 FIL file;
 
 void print_at(int column, int line, const char *msg)
 {
-	strcpy(&vram_char[line][column],msg); 
+	strcpy(&vram_char[line][column],msg);
 }
 
-static inline int min (int x, int y) 
-{ 
+static inline int min (int x, int y)
+{
 	return x<=y?x:y;
 }
 
@@ -131,20 +131,20 @@ static inline int min (int x, int y)
 // replace with full ascii art ?
 void window (int x1, int y1, int x2, int y2 )
 {
-	for (int i=x1+1;i<x2;i++) 
+	for (int i=x1+1;i<x2;i++)
 	{
 		vram_char[y1][i]='\xCD';
 		vram_char[y2][i]='\xCD';
 	}
-	for (int i=y1+1;i<y2;i++) 
+	for (int i=y1+1;i<y2;i++)
 	{
 		vram_char[i][x1]='\xBA';
 		vram_char[i][x2]='\xBA';
 	}
 	vram_char[y1][x1] ='\xC9';
-	vram_char[y1][x2] ='\xBB'; 
-	vram_char[y2][x1] ='\xC8'; 
-	vram_char[y2][x2] ='\xBC'; 
+	vram_char[y1][x2] ='\xBB';
+	vram_char[y2][x1] ='\xC8';
+	vram_char[y2][x2] ='\xBC';
 }
 
 
@@ -160,7 +160,7 @@ void list_roms()
     FRESULT res;
     FILINFO fno;
     DIR dir;
-    
+
     char *fn;   /* This function is assuming non-Unicode cfg. */
 #if _USE_LFN
     static char lfn[_MAX_LFN + 1];   /* Buffer to store the LFN */
@@ -180,23 +180,23 @@ void list_roms()
 #else
             fn = fno.fname;
 #endif
-            if (!(fno.fattrib & AM_DIR)) { // not a dir                  
+            if (!(fno.fattrib & AM_DIR)) { // not a dir
             	// assumes non LFN
-            	#if _USE_LFN 
-            	dslkhgsldkg 
+            	#if _USE_LFN
+            	dslkhgsldkg
             	#endif
 
             	// check extension : only keep .bin
             	if (strstr(fn,".BIN") || strstr(fn,".bin")) { // search ignoring case
                 	strcpy(filenames[nb_files],fn);
-                	nb_files +=1;            		
+                	nb_files +=1;
             	}
             }
         }
         if (res != FR_OK) {
 	        print_at(MSG_X,MSG_Y,"Error reading directory !");
     	    vram_char[MSG_Y+1][MSG_X] = '0'+res;
-        } 
+        }
         f_closedir(&dir);
     } else {
         print_at(MSG_X,MSG_Y,"Error opening directory !");
@@ -207,14 +207,14 @@ void list_roms()
     qsort(filenames, nb_files, 13, cmp);
 
 }
- 
+
 // read icon PBM data to memory
 int read_icon(const char *filename)
 {
 	char c;
 	FRESULT res;
 	UINT b_read;
-	
+
 	res = f_open (&file, filename, FA_READ);
 	if (res != FR_OK)
 		return res;
@@ -222,33 +222,33 @@ int read_icon(const char *filename)
 	// header
 	for (int i=0;i<3;i++) {
 		res = f_read (&file, &c, 1, &b_read);
-		if (res != FR_OK) 
+		if (res != FR_OK)
 			return res;
 		if (c!="P4\n"[i])
 			return 1000; // bad header
 	}
-	 
-	// skip one non-comment line  
+
+	// skip one non-comment line
 	int lines=0;
 	int cmt=0;
 	do {
 		res = f_read (&file, &c, 1,&b_read); // read first line char
 		cmt = (c=='#'); // asserts no empty line
-		while (c!='\n') 
+		while (c!='\n')
 			f_read (&file, &c, 1, &b_read);
 			// XXX check size 128
 		if (!cmt) lines += 1; // in a comment ?
 	} while (lines < 1 || cmt); // skip lines
-	 
+
 	res=f_read(&file,icon_data,ICON_SIZE,&b_read);
 	f_close(&file);
 	return res;
 }
 
 void default_icon( void )
-{	
-	uint8_t *p = bitbox_pbm;
-	for (int i=0;i<3;i++) 
+{
+	uint8_t *p = (uint8_t*)bitbox_pbm;
+	for (int i=0;i<3;i++)
 		while(*p++ != '\n'); // skip line
 	memcpy(icon_data, p, sizeof(icon_data));
 }
@@ -266,13 +266,13 @@ void game_init() {
 	FRESULT r = f_mount(&fs32,"",1); // mount now
 	if (r != FR_OK) {
 		print_at(5,20,"Cannot mount disk");
-		die(MOUNT,r); 
+		die(MOUNT,r);
 	}
-	
+
 	memset(icon_data, 0x55, sizeof(icon_data));
 	icon_x = 400;
 	icon_y= VGA_V_PIXELS / 2 - 80;
-	
+
 	default_icon();
 
 	list_roms();
@@ -287,7 +287,7 @@ int x =5,y=10 , dir_x=1, dir_y=1;
 char old_val=' ';
 char icon_name[13];
 
-void game_frame() 
+void game_frame()
 {
 	// interpret keyboard as gamepad & discard all other events
 	kbd_emulate_gamepad();
@@ -310,24 +310,24 @@ void game_frame()
 	if (!nb_files) return; // no need to go further
 
 
-	// handle input 
-	if (GAMEPAD_PRESSED(0,down) && vga_frame%4==0 && selected+offset < nb_files-1) 
+	// handle input
+	if (GAMEPAD_PRESSED(0,down) && vga_frame%4==0 && selected+offset < nb_files-1)
 	{
-		if (selected<DISPLAY_LINES-1) 
+		if (selected<DISPLAY_LINES-1)
 			selected +=1;
-		else 
+		else
 			offset += 1;
 	}
 
 
-	if (GAMEPAD_PRESSED(0,up) && vga_frame%4==0 && selected+offset > 0 ) 
+	if (GAMEPAD_PRESSED(0,up) && vga_frame%4==0 && selected+offset > 0 )
 	{
-		if (selected>0) 
+		if (selected>0)
 			selected -=1;
-		else 
+		else
 			offset -= 1;
 	}
-	
+
 	if (old_selected != selected) // only if there ARE files to display
 	{
 		strcpy(icon_name, filenames[selected]);
@@ -337,7 +337,7 @@ void game_frame()
 		if (r!=FR_OK)
 			default_icon();
 	}
-	
+
 	// Start flashing ?
 	if ((GAMEPAD_PRESSED(0,start) || GAMEPAD_PRESSED(0,A)))
 	{
@@ -351,8 +351,8 @@ void game_frame()
 			} else {
 				print_at(MSG_X,MSG_Y,"Error reading ");
 				print_at(MSG_X+14,MSG_Y,filename);
-			}		
-		} 
+			}
+		}
 	}
 
 
@@ -363,9 +363,9 @@ void game_frame()
 		char *s=filenames[offset+i];
 		for (l=0;s[l]!='.';l++)
 			vram_char[i+LIST_Y][10+l]=s[l];
-		for (;l<13;l++)	
+		for (;l<13;l++)
 			vram_char[i+LIST_Y][10+l]=' ';
-		// cursor 
+		// cursor
 		vram_char[LIST_Y+i][8]=(i==selected)?0x10:' ';
 		vram_char[LIST_Y+i][25]=(i==selected)?0x11:' ';
 	}
@@ -375,7 +375,7 @@ void game_frame()
 
 	memset(&vram_char[MSG_Y+1][MSG_X],' ',30);
 	strcpy(&vram_char[MSG_Y+1][MSG_X],flash_message);
-	flash_frame(); // at the end to let it finish 
+	flash_frame(); // at the end to let it finish
 }
 
 
@@ -384,7 +384,7 @@ extern const uint16_t bg_data[256];
 
 void graph_frame() {}
 
-void graph_line() 
+void graph_line()
 {
 	static uint32_t lut_data[4];
 	// text mode
@@ -395,7 +395,7 @@ void graph_line()
 	lut_data[1] = line_color<<16;
 	lut_data[2] = (uint32_t) line_color;
 	lut_data[3] = line_color * 0x10001;
-	
+
 
 	uint32_t *dst = (uint32_t *) draw_buffer;
 	char c;
