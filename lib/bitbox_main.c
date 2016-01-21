7edce09e471bb7e467465f45c71e63d949e64363
Makapuf <makapuf2@gmail.com>
2016-01-22 00:14:05 +0100

    whitespace

--- lib/bitbox_main.c
+++ lib/bitbox_main.c
@@ -1,16 +1,16 @@
 #include "bitbox.h"
 
-__attribute__((weak)) int main(void) 
+__attribute__((weak)) int main(void)
 {
 	game_init();
 
 	while (1) {
 		game_frame();
-		
+
 		// wait next frame.
 		int oframe=vga_frame;
 		while (oframe==vga_frame);
-			
+
 		set_led(button_state());
-	} 
+	}
 }
