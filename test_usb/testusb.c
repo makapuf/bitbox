3f348e2b9fb8c154d8854a6ccfc754b9122aa92f
Makapuf <makapuf2@gmail.com>
2016-06-18 14:05:41 +0200

    make testusb use mode 13
    allow making it work on bitbox micro

--- test_usb/testusb.c
+++ test_usb/testusb.c
@@ -7,20 +7,25 @@
 #include "bitbox.h"
 #include "simple.h"
 
+// graphical gamepad position 
+#define PAD_X 2
+#define PAD_Y 10
+#define KB_Y 20 // keyboard press positions
 int cx, cy;
 
 void game_init() {
 	clear(); 
 	
-	window(2,2,78,4);
-	print_at(17,3, " BITBOX USB TEST \x01 Hi ! Plug some usb device...");
-	print_at(1, 6, "Mouse: X=   Y=   lmr");
+	window(2,1,45,4);
+	print_at(14,2, " \xf9\xfa\xfb USB TEST ");
+	print_at(5,3, " \x01 Hi ! Plug some usb device...");
+	print_at(2, 6, "Mouse: X=   Y=   lmr");
 
-	print_at(1, 7, "Gamepad:");
+	print_at(PAD_X+2,PAD_Y-1, "Gamepad:");
 	// "graphical" gamepad
-	window(8, 8, 8+17, 12);
-	print_at(10,8,"[ ]");
-	print_at(4+17,8,"[ ]");
+	window(PAD_X, PAD_Y, PAD_X+17, PAD_Y+5);
+	print_at(PAD_X+2 ,PAD_Y,"[ ]");
+	print_at(PAD_X+15,PAD_Y,"[ ]");
 
 	// analog values
 	window(27, 6, 27+17, 6+9);
@@ -39,17 +44,17 @@ void display_first_event(void)
 		switch(e.type)
 		{
 			case evt_keyboard_press : 
-				print_at(1,15,"KB pressed      ");
-				vram[15][14]=HEX_Digits[(e.kbd.key>>4) & 0xF];
-				vram[15][15]=HEX_Digits[e.kbd.key&0xf];
-				vram[15][17]=e.kbd.sym;
+				print_at(1,KB_Y,"KB pressed      ");
+				vram[KB_Y][14]=HEX_Digits[(e.kbd.key>>4) & 0xF];
+				vram[KB_Y][15]=HEX_Digits[e.kbd.key&0xf];
+				vram[KB_Y][17]=e.kbd.sym;
 				break;
 
 			case evt_keyboard_release : 
-				print_at(1,15,"KB released     ");
-				vram[15][14]=HEX_Digits[(e.kbd.key>>4) & 0xF];
-				vram[15][15]=HEX_Digits[e.kbd.key&0xf];
-				vram[15][17]=e.kbd.sym;
+				print_at(1,KB_Y,"KB released     ");
+				vram[KB_Y][14]=HEX_Digits[(e.kbd.key>>4) & 0xF];
+				vram[KB_Y][15]=HEX_Digits[e.kbd.key&0xf];
+				vram[KB_Y][17]=e.kbd.sym;
 				break;
 
 			case evt_device_change:
@@ -65,11 +70,11 @@ void display_first_event(void)
 				break;
 
 			case evt_mouse_move:
-				vram[6][10]=HEX_Digits[(e.mov.x>>4) & 0xF];
-				vram[6][11]=HEX_Digits[e.mov.x&0xf];
+				vram[6][11]=HEX_Digits[(e.mov.x>>4) & 0xF];
+				vram[6][12]=HEX_Digits[e.mov.x&0xf];
 
-				vram[6][15]=HEX_Digits[(e.mov.y>>4) & 0xF];
-				vram[6][16]=HEX_Digits[e.mov.y&0xf];
+				vram[6][16]=HEX_Digits[(e.mov.y>>4) & 0xF];
+				vram[6][17]=HEX_Digits[e.mov.y&0xf];
 
 				cx += e.mov.x;
 				cy += e.mov.y;
@@ -81,15 +86,15 @@ void display_first_event(void)
 
 			case evt_mouse_click:
 				// the buttons IDs are NOT mousebut_left/right/middle...
-				if (e.button.id == 0) vram[6][18] = 'L';
-				if (e.button.id == 2) vram[6][19] = 'M';
-				if (e.button.id == 1) vram[6][20] = 'R';
+				if (e.button.id == 0) vram[6][19] = 'L';
+				if (e.button.id == 1) vram[6][20] = 'M';
+				if (e.button.id == 2) vram[6][21] = 'R';
 				break;
 
 			case evt_mouse_release:
-				if (e.button.id == 0) vram[6][18] = 'l';
-				if (e.button.id == 2) vram[6][19] = 'm';
-				if (e.button.id == 1) vram[6][20] = 'r';
+				if (e.button.id == 0) vram[6][19] = 'l';
+				if (e.button.id == 1) vram[6][20] = 'm';
+				if (e.button.id == 2) vram[6][21] = 'r';
 				break;
 
 			case evt_user:
@@ -117,20 +122,20 @@ void game_frame()
 	display_first_event();
 
 	// gamepad buttons
-	vram[9][11] = GAMEPAD_PRESSED(0,up) ? 'U':'u';
-	vram[11][11] = GAMEPAD_PRESSED(0,down) ? 'D':'d';
-	vram[10][9] = GAMEPAD_PRESSED(0,left) ? 'L':'l';
-	vram[10][13] = GAMEPAD_PRESSED(0,right) ? 'R':'r';
+	vram[PAD_Y+1][PAD_X+3] = GAMEPAD_PRESSED(0,up) ? 'U':'u';
+	vram[PAD_Y+3][PAD_X+3] = GAMEPAD_PRESSED(0,down) ? 'D':'d';
+	vram[PAD_Y+2][PAD_X+1] = GAMEPAD_PRESSED(0,left) ? 'L':'l';
+	vram[PAD_Y+2][PAD_X+5] = GAMEPAD_PRESSED(0,right) ? 'R':'r';
 
-	vram[10][24] = GAMEPAD_PRESSED(0,A) ? 'A':'a';
-	vram[11][22] = GAMEPAD_PRESSED(0,B) ? 'B':'b';
-	vram[9][22] = GAMEPAD_PRESSED(0,X) ? 'X':'x';
-	vram[10][20] = GAMEPAD_PRESSED(0,Y) ? 'Y':'y';
+	vram[PAD_Y+2][PAD_X+16] = GAMEPAD_PRESSED(0,A) ? 'A':'a';
+	vram[PAD_Y+3][PAD_X+14] = GAMEPAD_PRESSED(0,B) ? 'B':'b';
+	vram[PAD_Y+1][PAD_X+14] = GAMEPAD_PRESSED(0,X) ? 'X':'x';
+	vram[PAD_Y+2][PAD_X+12] = GAMEPAD_PRESSED(0,Y) ? 'Y':'y';
 
-	vram[8][11] = GAMEPAD_PRESSED(0,L) ? 'L':'l';
-	vram[8][22] = GAMEPAD_PRESSED(0,R) ? 'R':'r';
-	vram[11][15] = GAMEPAD_PRESSED(0,select) ? 'S':'s';
-	vram[11][18] = GAMEPAD_PRESSED(0,start) ? 'G':'g';
+	vram[PAD_Y  ][PAD_X+ 3] = GAMEPAD_PRESSED(0,L) ? 'L':'l';
+	vram[PAD_Y  ][PAD_X+14] = GAMEPAD_PRESSED(0,R) ? 'R':'r';
+	vram[PAD_Y+3][PAD_X+ 9] = GAMEPAD_PRESSED(0,select) ? 'S':'s';
+	vram[PAD_Y+3][PAD_X+10] = GAMEPAD_PRESSED(0,start) ? 'G':'g';
 
 	// analog gamepad
 	vram[5][40]=HEX_Digits[(gamepad_x[0]>>4) & 0xF];
@@ -139,7 +144,7 @@ void game_frame()
 	vram[5][43]=HEX_Digits[(gamepad_y[0]>>4) & 0xF];
 	vram[5][44]=HEX_Digits[gamepad_y[0]&0xf];
 
-	vram[11 + gpy / 32][36 + gpx / 16] = ' ';
+	vram[11 + gpy / 32][36  + gpx / 16] = ' ';
 
 	gpx = gamepad_x[0];
 	gpy = gamepad_y[0];
