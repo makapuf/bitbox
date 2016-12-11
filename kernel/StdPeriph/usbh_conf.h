ebf5e921eaeb4f43f9f1eca114a13b6ff7308564
Makapuf <makapuf2@gmail.com>
2016-12-11 23:18:33 +0100

    changed USB int priority back

--- kernel/StdPeriph/usbh_conf.h
+++ kernel/StdPeriph/usbh_conf.h
@@ -2,5 +2,5 @@
 
 #define USBH_MAX_NUM_ENDPOINTS                2
 #define USBH_MAX_NUM_INTERFACES               2
-#define USBH_MSC_MPS_SIZE                 0x40
+#define USBH_MSC_MPS_SIZE                 0x200
 
