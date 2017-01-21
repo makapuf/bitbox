0ca339e274cc822eb42d2d0aede447f62d0cbb21
Adrien Destugues <pulkomandy@pulkomandy.tk>
2017-01-21 23:32:38 +0100

    Split the chiptune engine and make oscillators public again
    I really need to access them directly in the tracker, and I don't see a
    point in hiding them behind some hardware abstraction thing.

--- lib/chiptune/chiptune.h
+++ lib/chiptune/chiptune.h
@@ -1,62 +1,51 @@
-/** Initialize the player to play the given song.
- * - Oscillators are reset to silence
- * - Song position is reset to 0
- * - Tracks and instruments are loaded from the song.
- *
- * Pass NULL to stop playing and not load a new song.
- */
 /* Simple soundengine for the BitBox
+ * Copyright 2015, Makapuf <makapuf2@gmail.com>
  * Copyright 2014, Adrien Destugues <pulkomandy@pulkomandy.tk>
  * Copyright 2007, Linus Akesson
- * Based on the "Hardware Chiptune" project */
+ * Based on the "Hardware Chiptune" project
+ *
+ * Low-level access to the oscllators.
+ */
 #pragma once
 #include <stdint.h>
 
-/*
-	Track is an 32 array of
-		0NNNNNNN ccccCCCC pppppppp PPPPPPPP
-		N : note (0 : no note, 7f : stop)
-		c : command1 (0-f)
-		C : command2 (0-f)
-		p : parameter1 (0-ff)
-		P : parameter2 (0-ff)
+#ifndef MAX_CHANNELS
+#define MAX_CHANNELS 8
+#endif
 
-	 0 : no command
-	 1: 0 = note off
-	 2: d = duty cycle
-	 3: f = volume slide
-	 4: i = inertia (auto note slides)
-	 5: j = instrument jump
-	 6: l = slide
-	 7: m = duty variation
-	 8: t = timing (or song speed)
-	 9: v = volume
-	 10: w = select waveform
-	 11: ~ = vibrato
-	 12: + = set relative note
-	 13: = = set absolute note (or instrument in pattern context)
-	 14: ! = set instrument
-
-
-that means that only one command 'f' is available unless we differentiate for instruments and track commands)
-*/
-
-struct ChipSong {
-	uint16_t songlen; // number of steps in the track sequencer
-	uint8_t numchannels; // 4 or 8 channels supported by now (number of tracks which can play simultaneously)
-	uint8_t tracklength; // 32 notes per track by default
-	uint8_t *tracklist; // id of tracks. songlen * numchannels
-	int8_t *transpose; // number of semitones. songlen * numchannels
-
-	uint16_t **instruments; // pointer to an array of pointers to command<<8 | parameter
-
-	uint32_t **tracks; // array of pointer to 32 u32 elements.
+// These are our possible waveforms. Any other value plays silence.
+enum {
+	WF_TRI, // triangle /\/\,
+	WF_SAW, // sawteeth /|/|,
+	WF_PUL, // pulse (adjustable duty) |_|-,
+	WF_NOI // noise !*@?
 };
 
-// init with a song. use 0 to stop playing current song (but keep it loaded !)
-void chip_play(const struct ChipSong *song);
+// This is the definition of our oscillators. There are 8 of these (4 for left,
+// 4 for right).
+struct oscillator {
+	uint16_t	freq; // frequency (except for noise, unused)
+	uint16_t	phase; // phase (except for noise, unused)
+	uint16_t	duty; // duty cycle (pulse wave only)
+	uint8_t	waveform; // waveform (from the enum above)
+	uint8_t	volume;	// 0-255
+	uint8_t bitcrush; // 0-f level of quantization (power of 2)
+};
 
-// play a note of this instrument now - useful for FX !
-void chip_note(uint8_t ch, uint8_t note, uint8_t instrument);
+// At each sample the phase is incremented by frequency/4. It is then used to
+// compute the output of the oscillator depending on the waveform.
+// This means the frequency unit is 65536*4/31000 or about 8.456Hz
+// and the frequency range is 0 to 554180Hz. Maybe it would be better to adjust
+// the scaling factor to allow less high frequencies (they are useless) but
+// more fine grained resolution. Not only we could play notes more in tune,
+// but also we would get a more subtle vibrato effect.
 
-int chip_song_playing();
+// ... and that's it for the engine, which is very simple as you see.
+// The parameters for the oscillators can be updated in your game_frame callback.
+// Since the audio buffer is generated in one go it is useless to try to tweak
+// the parameters more often than that.
+
+
+extern struct oscillator osc[MAX_CHANNELS];
+
+uint16_t gen_sample();
---
+++ lib/chiptune/player.h
@@ -0,0 +1,55 @@
+/* Music player for the chiptune engine
+ * Copyright 2014, Adrien Destugues <pulkomandy@pulkomandy.tk>
+ * Copyright 2007, Linus Akesson
+ * Based on the "Hardware Chiptune" project */
+#pragma once
+#include <stdint.h>
+
+/*
+	Track is an 32 array of
+		0NNNNNNN ccccCCCC pppppppp PPPPPPPP
+		N : note (0 : no note, 7f : stop)
+		c : command1 (0-f)
+		C : command2 (0-f)
+		p : parameter1 (0-ff)
+		P : parameter2 (0-ff)
+
+	 0 : no command
+	 1: 0 = note off
+	 2: d = duty cycle
+	 3: f = volume slide
+	 4: i = inertia (auto note slides)
+	 5: j = instrument jump
+	 6: l = slide
+	 7: m = duty variation
+	 8: t = timing (or song speed)
+	 9: v = volume
+	 10: w = select waveform
+	 11: ~ = vibrato
+	 12: + = set relative note
+	 13: = = set absolute note (or instrument in pattern context)
+	 14: ! = set instrument
+
+
+that means that only one command 'f' is available unless we differentiate for instruments and track commands)
+*/
+
+struct ChipSong {
+	uint16_t songlen; // number of steps in the track sequencer
+	uint8_t numchannels; // 4 or 8 channels supported by now (number of tracks which can play simultaneously)
+	uint8_t tracklength; // 32 notes per track by default
+	uint8_t *tracklist; // id of tracks. songlen * numchannels
+	int8_t *transpose; // number of semitones. songlen * numchannels
+
+	uint16_t **instruments; // pointer to an array of pointers to command<<8 | parameter
+
+	uint32_t **tracks; // array of pointer to 32 u32 elements.
+};
+
+// init with a song. use 0 to stop playing current song (but keep it loaded !)
+void chip_play(const struct ChipSong *song);
+
+// play a note of this instrument now - useful for FX !
+void chip_note(uint8_t ch, uint8_t note, uint8_t instrument);
+
+int chip_song_playing();
