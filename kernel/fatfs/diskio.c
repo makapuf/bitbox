b295afc59ac37ccee01ac457dc8d41005c207962
Makapuf <makapuf2@gmail.com>
2017-07-25 21:06:24 +0200

    update LFN support for boot2

--- kernel/fatfs/diskio.c
+++ kernel/fatfs/diskio.c
@@ -207,7 +207,31 @@ DRESULT disk_write (
 #endif /* _READONLY */
 
 
+// misc pseudo unicode 
+WCHAR ff_convert (WCHAR wch, UINT dir) 
+{ 
+          if (wch < 0x80) { 
+                    /* ASCII Char */ 
+                    return wch; 
+          }  
 
+          /* I don't support unicode it is too big! */ 
+          return 0; 
+}  
+
+WCHAR ff_wtoupper (WCHAR wch) 
+{ 
+          if (wch < 0x80) {      
+                    /* ASCII Char */ 
+                    if (wch >= 'a' && wch <= 'z') { 
+                              wch &= ~0x20; 
+                     } 
+                      return wch; 
+          }  
+
+          /* I don't support unicode it is too big! */ 
+          return 0; 
+} 
 
 /*-----------------------------------------------------------------------*/
 /* Miscellaneous Functions                                               */
